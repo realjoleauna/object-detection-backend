dc5d42bf61ff90fdb327a80d89dfd9d9ee715500
Gabriel Sampaio <gsampaio@redhat.com>
2023-07-30 19:57:26 -0300

    adding detected objects

--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,16 @@ const video = document.getElementById('video');
 const canvasOverlay = document.getElementById('canvas-overlay');
 const capturedPhoto = document.getElementById('captured-photo');
 
-const displayImages = (base64img) => {
+const displayImages = (base64img, detections) => {
     const img = new Image();
-    img.src = base64img;
+    const blob = dataURLtoBlob(`data:image/jpeg;base64,${base64img}`);
+    img.src = URL.createObjectURL(blob);
     capturedPhoto.innerHTML = '';
     capturedPhoto.appendChild(img);
+
+    if (detections) {
+        drawCircles(detections);
+    }
 };
 
 const startCamera = async () => {
@@ -53,14 +58,37 @@ const sendImageToServer = (base64img) => {
         const responseText = JSON.stringify(data, null, 2);
         console.log(responseText);
         // Update the UI with the detected image (if applicable)
-        if (data && data['image']) {
-            displayImages(data['image']);
+        if (data && data['detections']) {
+            displayImages(base64img, data['detections']);
         } else {
             // If no image is detected, display an error message
             capturedPhoto.innerHTML = '<p>No object detected in the captured photo.</p>';
         }
     })
     .catch(error => console.error('Error:', error));
+};
+
+const drawCircles = (detections) => {
+    const canvas = document.getElementById('canvas-overlay');
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    detections.forEach(detection => {
+        const { yMin, xMin, yMax, xMax } = detection.box;
+        const x = xMin * canvas.width;
+        const y = yMin * canvas.height;
+        const width = (xMax - xMin) * canvas.width;
+        const height = (yMax - yMin) * canvas.height;
+
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = 'red';
+        ctx.rect(x, y, width, height);
+        ctx.stroke();
+        ctx.font = '16px Arial';
+        ctx.fillStyle = 'red';
+        ctx.fillText(detection.label + ' (' + (detection.score * 100).toFixed(2) + '%)', x, y - 5);
+    });
 };
 
 // Start the camera when the page loads
